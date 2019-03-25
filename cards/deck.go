e6bf34678f168762ec2b7e59ab5549835a0d314a
En Zhou <zhouen.nathan@gmail.com>
2019-03-25 23:44:25 +0200

    Started the cards project

---
+++ cards/deck.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+// Create a new type of 'deck'
+// which is a slice of strings
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "One", "Two", "Three"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+//  Return 2 values with the same deck type
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
