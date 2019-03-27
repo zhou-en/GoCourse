f11b83b5cfc06c3645eed5ac2cf1f24533aa5260
En Zhou <zhouen.nathan@gmail.com>
2019-03-27 06:38:43 +0200

    Added new func

--- cards/main.go
+++ cards/main.go
@@ -1,10 +1,14 @@
 package main
 
+// import "fmt"
+
 func main() {
 	cards := newDeck()
+	cards.shuffle()
+	cards.print()
+	// hand, remainingCards := deal(cards, 5)
 
-	hand, remainingCards := deal(cards, 5)
+	// hand.print()
+	// remainingCards.print()
 
-	hand.print()
-	remainingCards.print()
 }
